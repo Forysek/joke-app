48899201a4c1beb2e5d06331e3e892b1d9c27707
Foryssek <foryssek@gmail.com>
2019-05-10 13:51:26 +0200

    adding bean and xml configuration

---
+++ src/main/java/com/forysek/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,10 @@
+package com.forysek.jokeapp.config;
+
+//@Configuration
+public class ChuckConfiguration {
+
+//    @Bean
+//    public ChuckNorrisQuotes chuckNorrisQuotes(){
+//        return new ChuckNorrisQuotes();
+//    }
+}
